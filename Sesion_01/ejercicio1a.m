c22c1b19abea0812dbe67db686afc8c403c61bf1
Oscar Hernandez <66411411+oscaarrhernandez@users.noreply.github.com>
2022-10-06 13:57:46 +0200

    Add files via upload

---
+++ Sesion_01/ejercicio1a.m
@@ -0,0 +1,8 @@
+%Ejercicio1 apartado a
+%oscarhernandez_70918137Y_sesion2
+A = 3;
+f = 10;
+fase = pi/4;
+t = 0:0.001:1;
+f=A*sin(2*pi*f*t+fase);
+plot(t,f);
---
+++ Sesion_01/ejercicio2c.m
@@ -0,0 +1,11 @@
+%Ejercicio2 apartado c
+%oscarhernandez_70918137Y_sesion2
+%function [w,coef]=rectangular(V,tau,T,tmin,tmax,m)
+V=5;
+tau=0.1;
+T=0.4;
+tmin=-2;
+tmax=2;
+m=10;
+[w,coef]=rectangular(V,tau,T,tmin,tmax,m);
+disp(coef);
