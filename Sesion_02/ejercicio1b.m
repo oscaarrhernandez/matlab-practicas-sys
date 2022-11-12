079e5a18aad88f1320c7e30c0148c86dd0e1ac0c
Oscar Hernandez <66411411+oscaarrhernandez@users.noreply.github.com>
2022-11-12 20:09:28 +0100

    Delete Sesion_02 directory

--- Sesion_02/ejercicio1b.m
+++
@@ -1,9 +0,0 @@
-% SESIÓN 3 SEÑALES Y SISTEMAS
-% Ejercicio 1
-% Apartado b
-% rectangular_c.m : [w,coef,t,y]=rectangular_c(V,tau,T,tmin,tmax,m,t0)
-
-[w,coef,t,y]=rectangular_c(5,0.002,0.02,-0.2,0.2,50,0);
-stem(w,coef,'-r');
-[w_esp,F] = espectro(t,y,0.02);
-plot(w_esp,F,'-b');
