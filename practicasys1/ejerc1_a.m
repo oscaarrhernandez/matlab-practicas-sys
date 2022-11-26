110bd7faf0dc18665be4518a1063adc330ce8438
Oscar Hernandez <66411411+oscaarrhernandez@users.noreply.github.com>
2022-11-26 18:53:20 +0100

    Delete practicasys1 directory

--- practicasys1/ejerc1_a.m
+++
@@ -1,9 +0,0 @@
-%Ejercicio1 apartado a
-%oscarhernandez_70918137Y_sesion2
-A=2;                %Amplitud
-f=20;               %Frecuencia
-fase=3*pi/4;        %Fase
-t=0:0.001:0.6;       %Tiempo entre 0 y 0.6 incrementando 0.01
-
-y = A*sin(2*pi*f*t+fase);
-plot(t,y);
